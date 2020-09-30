a0484b9b6673a42c3b3f899b38cd1105f5a2c9b4
Gabriel Robaina <49736266+gpr-indevelopment@users.noreply.github.com>
2020-09-30 16:25:59 -0300

    Spring initializr

---
+++ src/main/java/com/springcloudrabbitmqdemo/SpringCloudRabbitmqDemoApplication.java
@@ -0,0 +1,13 @@
+package com.springcloudrabbitmqdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudRabbitmqDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudRabbitmqDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springcloudrabbitmqdemo/SpringCloudRabbitmqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcloudrabbitmqdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudRabbitmqDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
